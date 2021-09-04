9e688730294bdcdaac31d2da21e54e41a2c00ae3
benroth <rothbenjamin503@gmail.com>
2021-09-03 20:16:23 -0400

    Completed hw 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Benjamin Roth
+hello.py
+
+Problem : Write a function that prints "hello, world!"
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+
+main()
